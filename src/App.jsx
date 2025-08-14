959d8584495b54b5780dfeec46805237abd8d6b3
Qofy <148817944+Qofy@users.noreply.github.com>
2025-08-14 14:50:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Home from "./pages/Home"
+import PageNotFound from "./pages/PageNotFound"
+import Pricing from "./pages/Pricing"
+import Product from "./pages/Product"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+
+function App() {
+  return (
+    
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="product" element={<Product/>}/>
+      <Route path="price" element={<Pricing/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
